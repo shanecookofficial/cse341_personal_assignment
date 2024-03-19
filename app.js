10528acbebc97630fc35589a2037455e8118b4ba
shanecookofficial <shanecookofficial@gmail.com>
2024-03-18 21:30:14 -0500

    Completed week 2 personal assignment

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
+const mongodb = require('./src/db/connect');
+
 const port = process.env.PORT || 3000;
+const app = express();
 
-app.use('/', require('./src/routes'));
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  })
+  .use('/', require('./src/routes'));
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+mongodb.initDb((err, mongodb) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
+  }
 });
